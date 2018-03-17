f60a30eee24c7f4f3123d2a8364dfd437bb0a5bb
Unknown <svenjanssen12321@gmail.com>
2018-03-17 12:46:01 +0100

    Started on tile raycast

---
+++ PersonalProject - Copy/Assets/Scripts/RaycastClicker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastClicker : MonoBehaviour {
+
+    public Camera cam;
+
+	// Use this for initialization
+	void Start ()
+    {
+        cam = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    
+}
